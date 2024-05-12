6e3d88238873aa284d51c098ee8146166e1e140b
Mateo Fernandez <mateo.rost@gmail.com>
2024-05-12 17:13:59 -0300

    Agrega interfaz Beer template del componente beer-list

--- src/app/beer-list/beer-list.component.ts
+++ src/app/beer-list/beer-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Beer } from './Beer';
 
 @Component({
   selector: 'app-beer-list',
@@ -6,11 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './beer-list.component.scss'
 })
 export class BeerListComponent {
-  beer = {
-    "name": "Porteña loca",
-    "type": "Porter",
-    "price": 3550,
-    "stock": 49,
-    "image": "assets/img/porter.jpg"
-  }
+  beers: Beer[] = [
+    {
+      name: "Porteña loca",
+      type: "Porter",
+      price: 3550,
+      stock: 49,
+      image: "assets/img/porter.jpg",
+      clearance: false,
+    },
+    {
+      name: "Lupulada",
+      type: "IPA",
+      price: 3900,
+      stock: 7,
+      image: "assets/img/porter.jpg",
+      clearance: true,
+    },
+    {
+      name: "Doradísima",
+      type: "Golden",
+      price: 2900,
+      stock: 0,
+      image: "assets/img/porter.jpg",
+      clearance: false,
+    },
+  ]; 
 }
